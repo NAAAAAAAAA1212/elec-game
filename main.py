decfa850b127747870932d232b60b4cd12be4ca2
Zeng Alexandre Qizhi <alexa@microsoft.zengqizhi.eu.org>
2024-05-02 19:28:49 +0800

    init

---
+++ main.py
@@ -0,0 +1,22 @@
+import pygame
+
+pygame.init()
+
+SCREEN_WIDTH = 800
+SCREEN_HEIGHT = int(SCREEN_WIDTH * 0.8)
+
+screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+screen.fill((255, 255, 155))
+
+
+run = True
+while run:
+    
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            run = False
+
+    pygame.display.update()
+
+pygame.quit()
