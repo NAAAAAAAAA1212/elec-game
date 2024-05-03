c7d68c8bba17bae6e3c6f3d3eca1254211ea9913
Zeng Alexandre Qizhi <alexa@microsoft.zengqizhi.eu.org>
2024-05-03 13:13:43 +0800

    adding classes

--- main.py
+++ main.py
@@ -1,18 +1,44 @@
 import pygame
-import pyautogui
-
-class sturcture:
-    def __init__(self):
-        pass
+import os
+import sys
 
 pygame.init()
 
-SCREEN_WIDTH, SCREEN_HEIGHT = pyautogui.size()
+SCREEN_WIDTH = 1000
+SCREEN_HEIGHT = 800
 
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
 screen.fill((255, 255, 255))
-pygame.display.set_caption('山海和一 電力供應局')
+pygame.display.set_caption('山海和一遊戲')
+
+class Structure:
+    def __init__(self):
+        self.image = None
+        self.rect = self.image.get_rect()
+        self.x = 0
+        self.y = 0
+
+class Button:
+    def __init__(self):
+        self.image = None
+        self.rect = self.image.get_rect()
+        self.x = 0
+        self.y = 0
+
+class GUI:
+    def __init__(self):
+        self.image = None
+        self.rect = self.image.get_rect()
+        self.x = 0
+        self.y = 0
+
+class Background:
+    def __init__(self):
+        self.image = None
+        self.rect = self.image.get_rect()
+        self.x = 0
+        self.y = 0
 
 run = True
 while run:
