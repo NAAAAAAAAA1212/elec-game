8bb03580d388e4b3ea3cf80fdf8b4d4a14c13a0e
Zeng Alexandre Qizhi <alexa@microsoft.zengqizhi.eu.org>
2024-05-03 22:02:47 +0800

    structer settings and upgrade system mainly built

--- main.py
+++ main.py
@@ -13,28 +13,47 @@ screen.fill((255, 255, 255))
 pygame.display.set_caption('山海和一遊戲')
 
 class Structure:
-    def __init__(self):
+    def __init__(self, image = int, name = str, high_level = int, cost = int, cost_per_upgrade = int) -> None:
+        self.image = image
+        self.name = name
+        self.high_level = high_level
+        self.cost = cost
+        self.cost_per_upgrade = cost_per_upgrade
+
+class Build:
+    def __init__(self, building = Structure(), level = int) -> None:
+        self.type = building
+        self.level = level
+        self.next_cost = self.type.cost + self.type.cost_per_upgrade
         self.image = None
         self.rect = self.image.get_rect()
         self.x = 0
         self.y = 0
 
+    def upgrade(self, money) -> bool:
+        if self.level == self.type.high_level and money >= self.next_cost:
+            return False
+        else:
+            self.level = self.level + 1
+            return True
+
+
 class Button:
-    def __init__(self):
+    def __init__(self) -> None:
         self.image = None
         self.rect = self.image.get_rect()
         self.x = 0
         self.y = 0
 
 class GUI:
-    def __init__(self):
+    def __init__(self) -> None:
         self.image = None
         self.rect = self.image.get_rect()
         self.x = 0
         self.y = 0
 
 class Background:
-    def __init__(self):
+    def __init__(self) -> None:
         self.image = None
         self.rect = self.image.get_rect()
         self.x = 0
@@ -50,4 +69,4 @@ while run:
     pygame.display.update()
 
 pygame.quit()
-#==
+
