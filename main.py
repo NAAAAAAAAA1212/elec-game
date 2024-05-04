763018ec36519e434a3f9925145feea56220f98f
jhugee333 <jhu333gee@gmail.com>
2024-05-04 10:32:51 +0800

    added full screen

--- main.py
+++ main.py
@@ -15,7 +15,7 @@ pygame.display.set_caption('山海和一遊戲')
 
 #class
 class Structure:
-    def __init__(self, image = int, name = str, high_level = int, cost = int, cost_per_upgrade = int):
+    def __init__(self, image = str, name = str, high_level = int, cost = int, cost_per_upgrade = int):
         self.image = image
         self.name = name
         self.high_level = high_level
@@ -42,28 +42,28 @@ class Build:
 
 class Button:
     def __init__(self, image, x = int, y = int):
-        self.image = image
+        self.image = pygame.image.load(image)
         self.rect = self.image.get_rect()
         self.x = x
         self.y = y
 
 class GUI:
     def __init__(self, image, x, y):
-        self.image = image
+        self.image = pygame.image.load(image)
         self.rect = self.image.get_rect()
         self.x = x
         self.y = y
 
 class Background:
     def __init__(self, image, x = int, y = int):
-        self.image = image
+        self.image = pygame.image.load(image)
         self.rect = self.image.get_rect()
         self.x = x
         self.y = y
 
 class BGtiles:
     def __init__(self, image, x = int, y = int):
-        self.image = image
+        self.image = pygame.image.load(image)
         self.rect = self.image.get_rect()
         self.x = x
         self.y = y
@@ -71,28 +71,27 @@ class BGtiles:
 
 
 #define
-def createTilesStr(filename = str):
-    return BGtiles("./assets/Tiles/tile_"+filename+".png")
-
-
-
 def createTileList():
     temp = []
-    for i in range(1, int(SCREEN_WIDTH/16) - 1):
-        for j in range(1, int(SCREEN_HEIGHT/16)):
+    for i in range(int(SCREEN_WIDTH/16)+1):
+        for j in range(int(SCREEN_HEIGHT/16)+1):
             temp.append([i,j])
-            ##pygame.draw.circle(screen, (0,0,0), (i*16+5,j*16+5), 5)
+            pygame.draw.circle(screen, (0,0,0), (i*16+5,j*16+5), 5)
     return temp
 
+def createTiles(list):
+    tempx = 0
+    for x in range(len(list)):
+        TILE = BGtiles("./assets/Tiles/tile_0002.png", tileList[tempx][0]*16, tileList[tempx][1]*16)
+        screen.blit(TILE.image, (TILE.x, TILE.y))
+        tempx += 1
 
 
 #variables
-
+TILE = None
 
 #lists
 tileList = createTileList()
-
-
 
 #run
 run = True
@@ -101,7 +100,8 @@ while run:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             run = False
-
+    #screen.blit(TILE.image, (TILE.x, TILE.y))
+    createTiles(tileList)
     pygame.display.update()
 
 pygame.quit()
