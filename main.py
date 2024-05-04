ff6f889f27c7561a049be330c8332de72c3dfb98
jhugee333 <jhu333gee@gmail.com>
2024-05-04 09:42:50 +0800

    lots of code

--- main.py
+++ main.py
@@ -1,17 +1,19 @@
 import pygame
-import os
-import sys
 
+
+#Tile pixel = 16x16
+#set
 pygame.init()
 
-SCREEN_WIDTH = 1000
-SCREEN_HEIGHT = 800
+SCREEN_WIDTH = 800
+SCREEN_HEIGHT = 600
 
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
 screen.fill((255, 255, 255))
 pygame.display.set_caption('山海和一遊戲')
 
+#class
 class Structure:
     def __init__(self, image = int, name = str, high_level = int, cost = int, cost_per_upgrade = int):
         self.image = image
@@ -21,14 +23,14 @@ class Structure:
         self.cost_per_upgrade = cost_per_upgrade
 
 class Build:
-    def __init__(self, building = Structure(), level = int):
+    def __init__(self, building = Structure(), level = int, x = int, y = int):
         self.type = building
         self.level = level
         self.next_cost = self.type.cost + self.type.cost_per_upgrade
         self.image = self.type.image
         self.rect = self.image.get_rect()
-        self.x = 0
-        self.y = 0
+        self.x = x
+        self.y = y
 
     def upgrade(self, money):
         if self.level == self.type.high_level and money >= self.next_cost:
@@ -39,26 +41,59 @@ class Build:
 
 
 class Button:
-    def __init__(self):
-        self.image = None
+    def __init__(self, image, x = int, y = int):
+        self.image = image
         self.rect = self.image.get_rect()
-        self.x = 0
-        self.y = 0
+        self.x = x
+        self.y = y
 
 class GUI:
-    def __init__(self):
-        self.image = None
+    def __init__(self, image, x, y):
+        self.image = image
         self.rect = self.image.get_rect()
-        self.x = 0
-        self.y = 0
+        self.x = x
+        self.y = y
 
 class Background:
-    def __init__(self):
-        self.image = None
+    def __init__(self, image, x = int, y = int):
+        self.image = image
         self.rect = self.image.get_rect()
-        self.x = 0
-        self.y = 0
+        self.x = x
+        self.y = y
 
+class BGtiles:
+    def __init__(self, image, x = int, y = int):
+        self.image = image
+        self.rect = self.image.get_rect()
+        self.x = x
+        self.y = y
+
+
+
+#define
+def createTiles():
+    pass
+
+def createTileList():
+    temp = []
+    for i in range(int(SCREEN_WIDTH/16)):
+        for j in range(int(SCREEN_HEIGHT/16)+1):
+            temp.append([i,j])
+            ##pygame.draw.circle(screen, (0,0,0), (i*16+5,j*16+5), 5)
+            
+    return temp
+
+
+
+#variables
+
+
+#lists
+tileList = createTileList()
+
+
+
+#run
 run = True
 while run:
     
