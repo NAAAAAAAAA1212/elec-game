417c66e582d20368850eb7e7207a4bc706521fe8
jhugee333 <jhu333gee@gmail.com>
2024-05-08 22:19:52 +0800

    update

--- launcher.py
+++ launcher.py
@@ -1,51 +1,55 @@
-'''
-import easygui
 
-open = easygui.buttonbox(msg = "開發者版本 1.0.0\n版權持有 (C) 2024 Zeng Alexandre Qizhi [alexa@microsoft.zengqizhi.eu.org] &\nJiang Bo Hong [jerry@microsoft.zengqizhi.eu.org]", title = "山海合一", choices = ["Start", "Stop"], cancel_choice = "Stop")
-if open is None:
-    open = "Stop"
-
-if open is "Start":
-    import main
-
-if open is "Stop":
-    exit
-'''
 import customtkinter 
 from tkinter import *
 from tkinter.ttk import *
 import time
+import easygui
 
 app = customtkinter.CTk()
-app.geometry("500x400")
+app.geometry("500x600")
 
 logined = False
 wrongEntry = 0
+secretpass = None
+
+
 def login():
-    if entry1.get() == "admin" and entry2.get() == "admin":
+    if entry1.get() == secretpass:
         global logined
         logined = True
         app.destroy()
+        import main
+        main.main()
     else:
         global wrongEntry
         wrongEntry += 1
         if wrongEntry <= 1:
-            label2 = customtkinter.CTkLabel(master=frame, text="用戶名稱或密碼錯誤", text_color="#FF0000")
+            label2 = customtkinter.CTkLabel(master=frame, text="密鑰錯誤", text_color="#FF0000")
             label2.pack(pady = 12, padx = 10)
+def getpass():
+    textbox = easygui.enterbox(msg = "")
+
+
 
 frame = customtkinter.CTkFrame(master = app)
 frame.pack(pady = 20, padx = 60, fill = "both", expand = True)
 
-label = customtkinter.CTkLabel(master = frame, text = "進入遊戲")
+label = customtkinter.CTkLabel(master = frame, text = "輸入密鑰進入遊戲")
 label.pack(pady = 12, padx = 10)
 
-entry1 = customtkinter.CTkEntry(master = frame, placeholder_text = "用戶名稱")
+entry1 = customtkinter.CTkEntry(master = frame, placeholder_text = "密鑰：")
 entry1.pack(pady = 12, padx = 10)
 
-entry2 = customtkinter.CTkEntry(master = frame, placeholder_text = "密碼", show = "*")
-entry2.pack(pady = 12, padx = 10)
+#entry2 = customtkinter.CTkEntry(master = frame, placeholder_text = "密碼", show = "*")
+#entry2.pack(pady = 12, padx = 10)
 
-button = customtkinter.CTkButton(master = frame, text = "登錄", command = login)
+button = customtkinter.CTkButton(master = frame, text = "進入遊戲", command = login)
+button.pack(pady = 12, padx = 10)
+
+label = customtkinter.CTkLabel(master = frame, text = "沒有密鑰？")
+label.pack(pady = 12, padx = 10)
+
+button = customtkinter.CTkButton(master = frame, text = "獲取密鑰", command = getpass)
 button.pack(pady = 12, padx = 10)
 
 label = customtkinter.CTkLabel(master=app, text="開發者版本 1.0.0\n版權持有 (C)\n2024 Zeng Alexandre Qizhi [alexa@microsoft.zengqizhi.eu.org] &\nJiang Bo Hong [jerry@microsoft.zengqizhi.eu.org]", text_color="#000000")
@@ -53,32 +57,3 @@ label.pack(pady = 12, padx = 10)
 
 app.mainloop()
 
-#print(logined)
-if logined:
-    def start():
-        progress = 0
-        speed = 1
-        while(progress < 100):
-            time.sleep(0.05)
-            bar['value']+=(speed/100)*100
-            progress += speed
-            window.update_idletasks()
-        window.destroy()
-
-
-    window = Tk()
-
-    window.title("Game Loading.....")
-
-    percent = StringVar()
-    text = StringVar()
-
-    bar = Progressbar(window,orient=HORIZONTAL,length=300)
-    bar.pack(pady=10)
-
-    start()
-
-    window.mainloop()
-
-
-import main
--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+'''
 import pygame
 import easygui
 
@@ -122,3 +123,136 @@ while run:
 pygame.quit()
 
 exit()
+'''
+import pygame
+import math
+def main(budget = int, infinity = bool, elec = float, envi = float):
+    pygame.init()
+    pygame.font.init()
+    FONT = pygame.font.Font('./assets/font/atop-font/Atop-R99O3.ttf', 70)
+    clock = pygame.time.Clock()
+    SCREEN_WIDTH = 800
+    SCREEN_HEIGHT = 600
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    screen.fill((255, 255, 255))
+    pygame.display.set_caption('山海和一遊戲')
+    class BGtiles:
+        def __init__(self, image, x = int, y = int):
+            self.image = pygame.image.load(image)
+            self.rect = self.image.get_rect()
+            self.x = x
+            self.y = y
+    #define
+    def createTileList():
+        temp = []
+        for i in range(int(SCREEN_WIDTH/16)+1):
+            for j in range(int(SCREEN_HEIGHT/16)+1):
+                temp.append([i,j])
+        return temp
+    def tileListInit():
+        temp = []
+        for x in range(len(tileposList)):
+            temp.append("0002")
+        return temp
+    def createTiles(list):
+        tempx = 0
+        for x in range(len(list)):
+            TILE = BGtiles("./assets/Tiles/tile_"+tilelist[tempx]+".png", tileposList[tempx][0]*16, tileposList[tempx][1]*16)
+            screen.blit(TILE.image, (TILE.x, TILE.y))
+            tempx += 1
+    def Showtext(font, text, color, x, y):
+        text = font.render(text, True, color)
+        textRect = text.get_rect(center = (x, y))
+        screen.blit(text, textRect)
+        
+    #variables
+    TILE = None
+    #lists
+    tileposList = createTileList()
+    tilelist = tileListInit()
+    #print(tileposList)
+    #run
+    run = True
+    createTiles(tileposList)
+    current = "2001"
+    elec_add = 100
+    envi_add = -60
+    elec_point = 0
+    envi_point = 100
+    cost = 300
+    while run:
+        events = pygame.event.get()
+        keys = pygame.key.get_pressed()
+        for event in events:
+            if event.type == pygame.QUIT:
+                run = False
+            if keys[pygame.K_1]:
+                current = "2001"
+                elec_add = 100
+                envi_add = -60
+                cost = 300
+            if keys[pygame.K_2]:
+                current = "2002"
+                elec_add = 200
+                envi_add = -100
+                cost = 400
+            if keys[pygame.K_3]:
+                current = "2003"
+                elec_add = 150
+                envi_add = -90
+                cost = 350
+            if keys[pygame.K_4]:
+                current = "2004"
+                elec_add = 10000
+                envi_add = -10
+                cost = 10000
+            if keys[pygame.K_5]:
+                current = "2005"
+                elec_add = 150
+                envi_add = 50
+                cost = 350
+            if keys[pygame.K_6]:
+                current = "2006"
+                elec_add = 200
+                envi_add = 100
+                cost = 500
+            if keys[pygame.K_7]:
+                current = "2007"
+                elec_add = 350
+                envi_add = 150
+                cost = 750
+            if keys[pygame.K_8]:
+                current = "2008"
+                elec_add = 500
+                envi_add = 300
+                cost = 1000
+            if keys[pygame.K_9]:
+                current = "2009"
+                elec_add = 5000
+                envi_add = 0
+                cost = 40000
+            if keys[pygame.K_0]:
+                break
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                posx, posy = pygame.mouse.get_pos()
+                if budget >= cost or infinity:
+                    TILE = BGtiles("./assets/Tiles/tile_" + current + ".png", posx/16*16-8, posy/16*16-8)
+                    
+                    screen.blit(TILE.image, (TILE.x, TILE.y))
+                    budget-=cost
+                    elec_point+=elec_add
+                    envi_point+=envi_add
+        Showtext(FONT, "署", (0,0,0), 100, 100)
+        pygame.display.update()
+        clock.tick(144)
+    pygame.quit()
+
+    point = 0.0
+    point = ((elec_point * elec / 100) + (envi_point * envi / 100)) * 10000000000
+    if infinity:
+        point /= 1000000
+    else:
+        point /= budget
+    return point
+
+main(9000, True, 50, 50)
