f3efac6d5e169df8790cc205f64d66a210c8a1c4
Zeng Alexandre Qizhi <alexa@microsoft.zengqizhi.eu.org>
2024-05-13 19:24:57 +0800

    revert AGAIN

--- launcher.py
+++ launcher.py
@@ -1,59 +1,235 @@
-
 import customtkinter 
 from tkinter import *
 from tkinter.ttk import *
 import time
-import easygui
+import activation
+import os
 
-app = customtkinter.CTk()
-app.geometry("500x600")
+reset = False
 
-logined = False
-wrongEntry = 0
-secretpass = None
+activation_raw = activation.check_activation()
+activation_status = False
+if activation_raw != "FAIL":
+    activation_status = True
 
+#activation
+while not activation_status:
+    wrongEntry = 0
+    def activate():
+        activation.write_activation(key.get(), type.get(), name.get(), mail.get())
+        activation_raw = activation.check_activation()
+        if activation_raw != "FAIL":
+            app.destroy()
+            global wrongEntry
+        else:
+            wrongEntry += 1
+            if wrongEntry <= 1:
+                label2 = customtkinter.CTkLabel(master=frame, text="激活碼錯誤", text_color="#FF0000")
+                label2.pack(pady = 12, padx = 10)
+    def leave():
+        exit()
+    app = customtkinter.CTk()
+    app.geometry("800x600")
+    frame = customtkinter.CTkFrame(master = app)
+    frame.pack(pady = 20, padx = 60, fill = "both", expand = True)
+    label = customtkinter.CTkLabel(master = frame, text = "激活遊戲")
+    label.pack(pady = 12, padx = 10)
+    key = customtkinter.CTkEntry(master = frame, placeholder_text = "激活碼")
+    key.pack(pady = 12, padx = 10)
+    name = customtkinter.CTkEntry(master = frame, placeholder_text = "全名")
+    name.pack(pady = 12, padx = 10)
+    mail = customtkinter.CTkEntry(master = frame, placeholder_text = "電郵")
+    mail.pack(pady = 12, padx = 10)
+    type = customtkinter.CTkEntry(master = frame, placeholder_text = "類型")
+    type.pack(pady = 12, padx = 10)
+    button = customtkinter.CTkButton(master = frame, text = "激活", command = activate)
+    button.pack(pady = 12, padx = 10)
+    button2 = customtkinter.CTkButton(master = frame, text = "激活", command = leave)
+    button2.pack(pady = 12, padx = 10)
+    label = customtkinter.CTkLabel(master=app, text="開發者版本 1.0.0\n版權持有 (C) 2024\nZeng Alexandre Qizhi [alexa@microsoft.zengqizhi.eu.org]\n&\nJiang Bo Hong [jerry@microsoft.zengqizhi.eu.org]", text_color="#000000")
+    label.pack(pady = 12, padx = 10)
+    app.mainloop()
+    activation_raw = activation.check_activation()
+    if activation_raw != "FAIL":
+        activation_status = True
 
-def login():
-    if entry1.get() == secretpass:
-        global logined
-        logined = True
+run = False
+if activation_status:
+    def start():
+        global run
+        run = True
         app.destroy()
-        import main
-        main.main()
-    else:
-        global wrongEntry
-        wrongEntry += 1
-        if wrongEntry <= 1:
-            label2 = customtkinter.CTkLabel(master=frame, text="密鑰錯誤", text_color="#FF0000")
-            label2.pack(pady = 12, padx = 10)
-def getpass():
-    textbox = easygui.enterbox(msg = "")
+    def leave():
+        exit()
+    def de():
+        global reset
+        reset = True
+        app.destroy()
+    app = customtkinter.CTk()
+    app.geometry("800x600")
+    frame = customtkinter.CTkFrame(master = app)
+    frame.pack(pady = 20, padx = 60, fill = "both", expand = True)
+    label1 = customtkinter.CTkLabel(master = frame, text = "山海合一")
+    label1.pack(pady = 12, padx = 10)
+    button1 = customtkinter.CTkButton(master = frame, text = "啟動", command = start)
+    button1.pack(pady = 12, padx = 10)
+    button2 = customtkinter.CTkButton(master = frame, text = "退出", command = leave)
+    button2.pack(pady = 12, padx = 10)
+    button3 = customtkinter.CTkButton(master = frame, text = "重置", command = de)
+    button3.pack(pady = 12, padx = 10)
+    label2 = customtkinter.CTkLabel(master=frame, text="激活版本: "+activation_raw, text_color="#000000")
+    label2.pack(pady = 12, padx = 10)
+    label3 = customtkinter.CTkLabel(master=app, text="開發者版本 1.0.0\n版權持有 (C) 2024\nZeng Alexandre Qizhi [alexa@microsoft.zengqizhi.eu.org]\n&\nJiang Bo Hong [jerry@microsoft.zengqizhi.eu.org]", text_color="#000000")
+    label3.pack(pady = 12, padx = 10)
+    app.mainloop()
 
+if not run:
+    exit()
 
+if reset:
+    def confirm():
+        os.remove("./activation/key.act")
+        os.remove("./activation/mail.act")
+        os.remove("./activation/name.act")
+        os.remove("./activation/type.act")
+        exit()
+    def leave():
+        exit()
+    app = customtkinter.CTk()
+    app.geometry("800x600")
+    frame = customtkinter.CTkFrame(master = app)
+    frame.pack(pady = 20, padx = 60, fill = "both", expand = True)
+    label1 = customtkinter.CTkLabel(master = frame, text = "重置所有設定")
+    label1.pack(pady = 12, padx = 10)
+    label2 = customtkinter.CTkLabel(master = frame, text = "警告: 這會清除激活碼！")
+    label2.pack(pady = 12, padx = 10)
+    button1 = customtkinter.CTkButton(master = frame, text = "確定", command = confirm)
+    button1.pack(pady = 12, padx = 10)
+    button2 = customtkinter.CTkButton(master = frame, text = "取消", command = leave)
+    button2.pack(pady = 12, padx = 10)
+    label2 = customtkinter.CTkLabel(master=frame, text="激活版本: "+activation_raw, text_color="#000000")
+    label2.pack(pady = 12, padx = 10)
+    label3 = customtkinter.CTkLabel(master=app, text="開發者版本 1.0.0\n版權持有 (C) 2024\nZeng Alexandre Qizhi [alexa@microsoft.zengqizhi.eu.org]\n&\nJiang Bo Hong [jerry@microsoft.zengqizhi.eu.org]", text_color="#000000")
+    label3.pack(pady = 12, padx = 10)
+    app.mainloop()
 
+budget = 0
+infinity_budget = False
+wrongEntry2 = 0
+while budget == 0 and not infinity_budget:
+    def leave():
+        exit()
+    def budget_set():
+        good = False
+        try:
+            global budget
+            budget = int(text1.get())
+            good = True
+        except:
+            global wrongEntry2
+            wrongEntry2 += 1
+            if wrongEntry2 <= 1:
+                label5 = customtkinter.CTkLabel(master=frame, text="預算輸入錯誤", text_color="#FF0000")
+                label5.pack(pady = 12, padx = 10)
+        if good:
+            app.destroy()
+    def infinity():
+        global infinity_budget
+        infinity_budget = True
+        app.destroy()
+    app = customtkinter.CTk()
+    app.geometry("800x600")
+    frame = customtkinter.CTkFrame(master = app)
+    label1 = customtkinter.CTkLabel(master = frame, text = "山海合一")
+    label1.pack(pady = 12, padx = 10)
+    label2 = customtkinter.CTkLabel(master = frame, text = "請輸入你想要的預算")
+    label2.pack(pady = 12, padx = 10)
+    frame = customtkinter.CTkFrame(master = app)
+    frame.pack(pady = 20, padx = 60, fill = "both", expand = True)
+    text1 = customtkinter.CTkEntry(master = frame, placeholder_text = "預算")
+    text1.pack(pady = 12, padx = 10)
+    button1 = customtkinter.CTkButton(master = frame, text = "我要這些預算", command = budget_set)
+    button1.pack(pady = 12, padx = 10)
+    button2 = customtkinter.CTkButton(master = frame, text = "我要無限預算", command = infinity)
+    button2.pack(pady = 12, padx = 10)
+    button3 = customtkinter.CTkButton(master = frame, text = "關閉遊戲", command = leave)
+    button3.pack(pady = 12, padx = 10)  
+    label3 = customtkinter.CTkLabel(master=frame, text="激活版本: "+activation_raw, text_color="#000000")
+    label3.pack(pady = 12, padx = 10)
+    label4 = customtkinter.CTkLabel(master=app, text="開發者版本 1.0.0\n版權持有 (C) 2024\nZeng Alexandre Qizhi [alexa@microsoft.zengqizhi.eu.org]\n&\nJiang Bo Hong [jerry@microsoft.zengqizhi.eu.org]", text_color="#000000")
+    label4.pack(pady = 12, padx = 10)
+    app.mainloop()
+wrongEntry2 = 0
+point_elec = 0.0
+point_newe = 0.0
+while point_elec == 0.0 and point_newe == 0.0:
+    app = customtkinter.CTk()
+    app.geometry("800x600")
+    def check():
+        global wrongEntry2
+        try:
+            if float(text1.get()) + float(text2.get()) == 100:
+                try:
+                    global point_elec
+                    global point_newe
+                    point_elec = float(text1.get())
+                    point_newe = float(text2.get())
+                    app.destroy()
+                except:
+                    wrongEntry2 += 1
+                    if wrongEntry2 <= 1:
+                        label5 = customtkinter.CTkLabel(master=frame, text="分數佔比輸入錯誤", text_color="#FF0000")
+                        label5.pack(pady = 12, padx = 10)
+            else:
+                wrongEntry2 += 1
+                if wrongEntry2 <= 1:
+                    label5 = customtkinter.CTkLabel(master=frame, text="分數佔比輸入錯誤", text_color="#FF0000")
+                    label5.pack(pady = 12, padx = 10)
+        except:
+            wrongEntry2 += 1
+            if wrongEntry2 <= 1:
+                label2 = customtkinter.CTkLabel(master=frame, text="分數佔比輸入錯誤", text_color="#FF0000")
+                label2.pack(pady = 12, padx = 10)
+    def leave():
+        exit()
+    frame = customtkinter.CTkFrame(master = app)
+    frame.pack(pady = 20, padx = 60, fill = "both", expand = True)
+    label1 = customtkinter.CTkLabel(master = frame, text = "山海合一")
+    label1.pack(pady = 12, padx = 10)
+    label2 = customtkinter.CTkLabel(master = frame, text = "請設置分數佔比")
+    label2.pack(pady = 12, padx = 10)
+    text1 = customtkinter.CTkEntry(master = frame, placeholder_text = "能源輸出佔比")
+    text1.pack(pady = 12, padx = 10)
+    text2 = customtkinter.CTkEntry(master = frame, placeholder_text = "環境輸出佔比")
+    text2.pack(pady = 12, padx = 10)
+    button1 = customtkinter.CTkButton(master = frame, text = "分數設置", command = check)
+    button1.pack(pady = 12, padx = 10)
+    button2 = customtkinter.CTkButton(master = frame, text = "關閉遊戲", command = leave)
+    button2.pack(pady = 12, padx = 10)  
+    label3 = customtkinter.CTkLabel(master=frame, text="激活版本: "+activation_raw, text_color="#000000")
+    label3.pack(pady = 12, padx = 10)
+    label4 = customtkinter.CTkLabel(master=app, text="開發者版本 1.0.0\n版權持有 (C) 2024\nZeng Alexandre Qizhi [alexa@microsoft.zengqizhi.eu.org]\n&\nJiang Bo Hong [jerry@microsoft.zengqizhi.eu.org]", text_color="#000000")
+    label4.pack(pady = 12, padx = 10)
+    app.mainloop()
+
+import main
+point = main.main(budget, infinity_budget, point_elec, point_newe)
+app = customtkinter.CTk()
+app.geometry("800x600")
+def bye():
+    exit()
 frame = customtkinter.CTkFrame(master = app)
 frame.pack(pady = 20, padx = 60, fill = "both", expand = True)
-
-label = customtkinter.CTkLabel(master = frame, text = "輸入密鑰進入遊戲")
-label.pack(pady = 12, padx = 10)
-
-entry1 = customtkinter.CTkEntry(master = frame, placeholder_text = "密鑰：")
-entry1.pack(pady = 12, padx = 10)
-
-#entry2 = customtkinter.CTkEntry(master = frame, placeholder_text = "密碼", show = "*")
-#entry2.pack(pady = 12, padx = 10)
-
-button = customtkinter.CTkButton(master = frame, text = "進入遊戲", command = login)
-button.pack(pady = 12, padx = 10)
-
-label = customtkinter.CTkLabel(master = frame, text = "沒有密鑰？")
-label.pack(pady = 12, padx = 10)
-
-button = customtkinter.CTkButton(master = frame, text = "獲取密鑰", command = getpass)
-button.pack(pady = 12, padx = 10)
-
-label = customtkinter.CTkLabel(master=app, text="開發者版本 1.0.0\n版權持有 (C)\n2024 Zeng Alexandre Qizhi [alexa@microsoft.zengqizhi.eu.org] &\nJiang Bo Hong [jerry@microsoft.zengqizhi.eu.org]", text_color="#000000")
-label.pack(pady = 12, padx = 10)
-
+label1 = customtkinter.CTkLabel(master = frame, text = "山海合一")
+label1.pack(pady = 12, padx = 10)
+label2 = customtkinter.CTkLabel(master = frame, text = "遊戲結束!")
+label2.pack(pady = 12, padx = 10)
+label3 = customtkinter.CTkLabel(master = frame, text = "你的分數是:")
+label3.pack(pady = 12, padx = 10)
+label4 = customtkinter.CTkLabel(master = frame, text = point)
+label4.pack(pady = 12, padx = 10)
+button1 = customtkinter.CTkButton(master = frame, text = ">>>結束<<<", command = bye)
+button1.pack(pady = 12, padx = 10)
+label5 = customtkinter.CTkLabel(master=app, text="開發者版本 1.0.0\n版權持有 (C) 2024\nZeng Alexandre Qizhi [alexa@microsoft.zengqizhi.eu.org]\n&\nJiang Bo Hong [jerry@microsoft.zengqizhi.eu.org]", text_color="#000000")
+label5.pack(pady = 12, padx = 10)
 app.mainloop()
-
--- main.py
+++ main.py
@@ -1,135 +1,6 @@
-'''
-import pygame
-import easygui
-
-#Tile pixel = 16x16
-#set
-pygame.init()
-
-#fps
-clock = pygame.time.Clock()
-
-SCREEN_WIDTH = 800
-SCREEN_HEIGHT = 600
-
-screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-
-screen.fill((255, 255, 255))
-pygame.display.set_caption('山海和一遊戲')
-
-#class
-class Structure:
-    def __init__(self, image = str, name = str, high_level = int, cost = int, cost_per_upgrade = int):
-        self.image = image
-        self.name = name
-        self.high_level = high_level
-        self.cost = cost
-        self.cost_per_upgrade = cost_per_upgrade
-
-class Build:
-    def __init__(self, building = Structure(), level = int, x = int, y = int):
-        self.type = building
-        self.level = level
-        self.next_cost = self.type.cost + self.type.cost_per_upgrade
-        self.image = self.type.image
-        self.rect = self.image.get_rect()
-        self.x = x
-        self.y = y
-
-    def upgrade(self, money):
-        if self.level == self.type.high_level and money >= self.next_cost:
-            return False
-        else:
-            self.level = self.level + 1
-            return True
-
-
-class Button:
-    def __init__(self, image, x = int, y = int):
-        self.image = pygame.image.load(image)
-        self.rect = self.image.get_rect()
-        self.x = x
-        self.y = y
-
-class GUI:
-    def __init__(self, image, x, y):
-        self.image = pygame.image.load(image)
-        self.rect = self.image.get_rect()
-        self.x = x
-        self.y = y
-
-class Background:
-    def __init__(self, image, x = int, y = int):
-        self.image = pygame.image.load(image)
-        self.rect = self.image.get_rect()
-        self.x = x
-        self.y = y
-
-class BGtiles:
-    def __init__(self, image, x = int, y = int):
-        self.image = pygame.image.load(image)
-        self.rect = self.image.get_rect()
-        self.x = x
-        self.y = y
-
-
-
-#define
-def createTileList():
-    temp = []
-    for i in range(int(SCREEN_WIDTH/16)+1):
-        for j in range(int(SCREEN_HEIGHT/16)+1):
-            temp.append([i,j])
-            #pygame.draw.circle(screen, (0,0,0), (i*16+5,j*16+5), 5)
-    return temp
-
-def tileListInit():
-    temp = []
-    for x in range(len(tileposList)):
-        temp.append("0002")
-    return temp
-
-def createTiles(list):
-    tempx = 0
-    for x in range(len(list)):
-        TILE = BGtiles("./assets/Tiles/tile_"+tilelist[tempx]+".png", tileposList[tempx][0]*16, tileposList[tempx][1]*16)
-        screen.blit(TILE.image, (TILE.x, TILE.y))
-        tempx += 1
-
-
-
-#variables
-TILE = None
-
-#lists
-tileposList = createTileList()
-tilelist = tileListInit()
-#run
-run = True
-createTiles(tileposList)
-while run:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            run = False
-        if event.type == pygame.MOUSEBUTTONDOWN:
-            posx, posy = pygame.mouse.get_pos()
-            print(pygame.mouse.get_pos())
-            TILE = BGtiles("./assets/Tiles/tile_0006.png", posx/16*16-8, posy/16*16-8)
-            screen.blit(TILE.image, (TILE.x, TILE.y))
-    #screen.blit(TILE.image, (TILE.x, TILE.y))
-    #createTiles(tileposList)
-    pygame.display.update()
-    clock.tick(144)
-pygame.quit()
-
-exit()
-'''
-import pygame
-import math
 def main(budget = int, infinity = bool, elec = float, envi = float):
+    import pygame
     pygame.init()
-    pygame.font.init()
-    FONT = pygame.font.Font('./assets/font/atop-font/Atop-R99O3.ttf', 70)
     clock = pygame.time.Clock()
     SCREEN_WIDTH = 800
     SCREEN_HEIGHT = 600
@@ -160,17 +31,11 @@ def main(budget = int, infinity = bool, elec = float, envi = float):
             TILE = BGtiles("./assets/Tiles/tile_"+tilelist[tempx]+".png", tileposList[tempx][0]*16, tileposList[tempx][1]*16)
             screen.blit(TILE.image, (TILE.x, TILE.y))
             tempx += 1
-    def Showtext(font, text, color, x, y):
-        text = font.render(text, True, color)
-        textRect = text.get_rect(center = (x, y))
-        screen.blit(text, textRect)
-        
     #variables
     TILE = None
     #lists
     tileposList = createTileList()
     tilelist = tileListInit()
-    #print(tileposList)
     #run
     run = True
     createTiles(tileposList)
@@ -237,16 +102,13 @@ def main(budget = int, infinity = bool, elec = float, envi = float):
                 posx, posy = pygame.mouse.get_pos()
                 if budget >= cost or infinity:
                     TILE = BGtiles("./assets/Tiles/tile_" + current + ".png", posx/16*16-8, posy/16*16-8)
-                    
                     screen.blit(TILE.image, (TILE.x, TILE.y))
                     budget-=cost
                     elec_point+=elec_add
                     envi_point+=envi_add
-        Showtext(FONT, "署", (0,0,0), 100, 100)
         pygame.display.update()
         clock.tick(144)
     pygame.quit()
-
     point = 0.0
     point = ((elec_point * elec / 100) + (envi_point * envi / 100)) * 10000000000
     if infinity:
@@ -254,5 +116,3 @@ def main(budget = int, infinity = bool, elec = float, envi = float):
     else:
         point /= budget
     return point
-
-main(9000, True, 50, 50)
