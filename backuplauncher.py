0b602a33d4874c344937a8e9c65fd1dbea7c237f
Zeng Alexandre Qizhi <alexa@microsoft.zengqizhi.eu.org>
2024-05-13 20:36:18 +0800

    wrting backup launcher

---
+++ backuplauncher.py
@@ -0,0 +1,37 @@
+print("開發者公測版本 1.0.0\n版權持有 (C) 2024\nZeng Alexandre Qizhi [alexa@microsoft.zengqizhi.eu.org]\n&\nJiang Bo Hong [jerry@microsoft.zengqizhi.eu.org]")
+print("要不要無限預算？ [Y/N]")
+unlimited_raw = "NA"
+while unlimited_raw != "Y" and unlimited_raw != "N":
+    unlimited_raw = input("[Y/N] >>> ")
+    if unlimited_raw == "Y":
+        unlimitwed = True
+    else:
+        unlimitwed = False
+budget = 0
+if not unlimited_raw:
+    print("要多少預算?")
+    while budget == 0:
+        unlimited_raw = input(" >>> ")
+        try:
+            budget = int(unlimited_raw)
+        except:
+            pass
+print("請輸入分數比例:")
+elec = 0.0
+newe = 0.0
+while elec == 0.0 and newe == 0.0 and elec + newe != 100:
+    elec_raw = input("elec >>> ")
+    newe_raw = input("newe >>> ")
+    try:
+        elec = float(elec_raw)
+        newe = float(newe_raw)
+    except:
+        pass
+print("-----------")
+print("starting game......")
+import main
+result = main.main(budget, unlimitwed, elec, newe)
+print("GAME ENDDED")
+print("-----------")
+print("你的分數是：" + str(result))
+input("按回車結束遊戲")
